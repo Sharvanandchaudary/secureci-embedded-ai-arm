b593b2324083a5505229637041077329a33622f5
veerapasala <saravana.vzf78@gmail.com>
2025-05-02 21:51:17 -0500

    Initial commit: STM32 firmware with Makefile and CI

---
+++ test/build/test/dependencies/test_sensors.d
@@ -0,0 +1,3 @@
+build/test/out/test_sensors/test_sensors.o: test/test_sensors.c \
+ build/vendor/unity/src/unity.h build/vendor/unity/src/unity_internals.h \
+ ../include/sensors.h
---
+++ test/build/test/dependencies/test_sensors_runner.d
@@ -0,0 +1,3 @@
+build/test/out/test_sensors/test_sensors_runner.o: \
+ build/test/runners/test_sensors_runner.c build/vendor/unity/src/unity.h \
+ build/vendor/unity/src/unity_internals.h ../include/sensors.h
